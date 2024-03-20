33651e7fe3e96630a14351b47247ca90663f873b
someone <onlyiqquee@gmail.com>
2024-03-20 12:13:34 +0100

    nigeria test for nin added

--- nigeria_test.go
+++ nigeria_test.go
@@ -1,1 +1,33 @@
 package qoreid
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_CreateService(t *testing.T) {
+	httpClient := http.DefaultClient
+	token := ""
+
+	client := New(httpClient, token)
+	assert.NotNil(t, client)
+	assert.NotNil(t, client.Nigeria)
+
+	payload := VerifyNinWithNinReq{
+		IdNumber:  "000000000",
+		Firstname: "jane",
+		Lastname:  "doe",
+	}
+
+	response, err := client.Nigeria.VerifyNinWithNin(payload)
+	if err != nil {
+		assert.Error(t, err)
+	}
+
+	// TODO cannot be nil
+	assert.Nil(t, response)
+}
+
+// TODO mock newRequest to accept custom request object and return custom request response
